8a0b7a7e4ebcd89d63ee94a333e6269f90d2a445
Skylar83 <37374070+Skylar83@users.noreply.github.com>
2021-10-27 02:03:38 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 import selenium.Facebook;
 import selenium.Network;
 import selenium.Twitter;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.io.BufferedReader;
 
 public class Main extends JFrame implements ActionListener {
     private Container c;
